666693c0bb7f53e5c32e992860869f1d1b1cebc7
Paulo Rafael <paulo.fsoft@hotmail.com>
2021-12-21 16:26:15 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 import './App.css';
 
+const rickMortyCharacters = [
+  {
+    id: 'rick',
+    name: 'Rick Sanchez',
+    thumb: '/images/Rick_Sanchez.png'
+  },
+  {
+    id: 'morty',
+    name: 'Morty Smith',
+    thumb: '/images/Morty501.png'
+  },
+  {
+    id: 'beth',
+    name: 'Beth Smith',
+    thumb: '/images/Beth_Smith.png'
+  },
+  {
+    id: 'summer',
+    name: 'Summer Smith',
+    thumb: '/images/Summer_is_cool.jpg'
+  },
+  {
+    id: 'jerry',
+    name: 'Jerry Smith',
+    thumb: '/images/Jerry_Smith.png'
+  },
+  {
+    id: 'birdperson',
+    name: 'Birdperson',
+    thumb: '/images/NewBirdPerson.jpg'
+  },
+];
+
 function App() {
+
+  const [characters, setCaracters] = useState(rickMortyCharacters);
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setCaracters(items);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Rick and Morty characters</h1>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {
+                characters.map(({ id, name, thumb }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li className="list" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                          <img src={thumb} alt={name} className="thumb" />
+                          <div className="name">{name}</div>
+                        </li>
+                      )}
+                    </Draggable>
+                  )
+                })
+              }
+              {provided.placeholder}
+            </ul>
+          )}
+
+        </Droppable>
+      </DragDropContext>
+    </div >
   );
 }
 
